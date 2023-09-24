1fef6e1bc97115cb7edd2946f82c60fc71997b7e
Korniichuk Alexandra <65713096+AlexandraKorniichuk@users.noreply.github.com>
2023-09-25 02:39:25 +0300

    Initial commit

---
+++ Source/Shizoid/ShizoidGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ShizoidGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AShizoidGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AShizoidGameMode();
+};
+
+
+
